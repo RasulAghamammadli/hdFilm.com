729fdfc012052b9dc96aa6cb57b33da2c840988d
Rasul0101 <rasul.aghamammadli@gmail.com>
2024-06-10 17:50:52 +0400

    Added Filter components(movie-robot,categories,years) to Home page

--- src/components/Filter/Categories.jsx
+++ src/components/Filter/Categories.jsx
@@ -1,9 +1,30 @@
 // import React from 'react'
+import { Link } from "react-router-dom";
+
+// data
+import { categories } from "../../categoryData.js";
+
+// Icons
+import { FaBars } from "react-icons/fa";
 
 const Categories = () => {
   return (
-    <div>Categories</div>
-  )
-}
+    <div className="categories">
+      <div className="title">
+        <h5 className="head">
+          <FaBars className="icon" />
+          Film Türleri
+        </h5>
+      </div>
+      <ul className="list">
+        {categories.map((item) => (
+          <li key={item} className="list-item">
+            <Link to={item}>{item}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default Categories
+export default Categories;
--- src/components/Filter/Years.jsx
+++ src/components/Filter/Years.jsx
@@ -1,9 +1,33 @@
 // import React from 'react'
+import { Link } from "react-router-dom";
+
+// Icons
+import { FaCalendarAlt } from "react-icons/fa";
 
 const Years = () => {
-  return (
-    <div>Years</div>
-  )
-}
+  // Years Arr
+  const years = [];
+  for (let year = 1969; year <= 2024; year++) {
+    years.push(year);
+  }
 
-export default Years
+  return (
+    <div className="years">
+      <div className="title">
+        <h5 className="head">
+          <FaCalendarAlt className="icon" />
+          Yıllara Göre Filmler
+        </h5>
+      </div>
+      <ul className="list">
+        {years.reverse().map((item) => (
+          <li key={item} className="list-item">
+            <Link to={item}>{item}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Years;
