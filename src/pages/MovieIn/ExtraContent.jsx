158eb2dcfebf968cc47018e8408d4386e2aa1f93
Rustamli510 <qedir.rustemli02@gmail.com>
2024-08-01 19:08:28 +0400

    movie-inner 40%

--- src/pages/MovieIn/ExtraContent.jsx
+++ src/pages/MovieIn/ExtraContent.jsx
@@ -1,8 +1,35 @@
-// import React from 'react'
+import React from 'react'
+
+// Icons
+import { FaInfoCircle } from "react-icons/fa";
 
 const ExtraContent = () => {
   return (
-    <div>ExtraContent</div>
+    <>
+      {/* Title */}
+      <div className='extra-content-title'>
+        <div className="title">
+          <div className="head">
+            <h5 className="head-text">
+              <FaInfoCircle className="icon" />
+              Film bilgileri
+            </h5>
+            <span className="head-text-right">
+              9 ay önce eklendi
+            </span>
+          </div>
+        </div>
+      </div>
+      {/* Title */}
+
+      {/* About Movie */}
+        <div className='about-movie'>
+          <div className=''>
+
+          </div>
+        </div>
+      {/* About Movie */}
+    </>
   )
 }
 
--- src/pages/MovieIn/SingleContent.jsx
+++ src/pages/MovieIn/SingleContent.jsx
@@ -1,12 +1,29 @@
-// import React from 'react'
+import React from 'react';
 
 // Icons
 import { FaCirclePlay } from "react-icons/fa6";
 import { FaRegLightbulb } from "react-icons/fa";
 import { FaFlag } from "react-icons/fa";
 import { FaPlusSquare } from "react-icons/fa";
+import YouTube from "react-youtube";
+import { FaShare } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+import { AiFillLike } from "react-icons/ai";
+import { AiFillDislike } from "react-icons/ai";
+//Icons
 
 const SingleContent = () => {
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      autoplay: 0,
+    },
+  };
+
   return (
     <>
       {/* Title */}
@@ -46,9 +63,41 @@ const SingleContent = () => {
       {/* Video top */}
 
       {/* Video */}
-        <video>
-          <source src="" />
-        </video>
+      <div>
+        <YouTube videoId="2J2iZcKIkk8" opts={opts} />
+      </div>
+      <div className='video-bottom'>
+        <div className="video-bottom-left">
+          <div className="share-left">
+            <p>Paylaş</p>
+            <FaShare className='share-icon' />
+          </div>
+          <div className='share-right'>
+            <Link to='https://www.facebook.com/' target='blank' className='social'>
+              <FaFacebookF />
+              <Link to='https://www.facebook.com/' target='blank'>Facebook</Link>
+            </Link>
+            <Link to='https://x.com/?mx=2' target='blank' className='social'>
+              <FaTwitter />
+              <Link to='https://x.com/?mx=2' target='blank'>Twitter</Link>
+            </Link>
+            <Link to='https://web.whatsapp.com/' target='blank' className='social'>
+              <FaWhatsapp />
+              <Link to='https://web.whatsapp.com/' target='blank'>WhatsApp</Link>
+            </Link>
+          </div>
+        </div>
+        <div className='video-bottom-right'>
+          <div className='like'>
+            <AiFillLike />
+            <button>Beğendim</button>
+          </div>
+          <div className='dislike'>
+            <AiFillDislike />
+            <button>Beğenmedim</button>
+          </div>
+        </div>
+      </div>
       {/* Video */}
     </>
   );
